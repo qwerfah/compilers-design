0bffc836b3a39c2071dc90c5515093213743b834
qwerfah <qwerfah@gmail.com>
2021-05-12 22:24:49 +0300

    Lexer updated to read from file.

--- LR3/src/main/scala/analysis/Lexer.scala
+++ LR3/src/main/scala/analysis/Lexer.scala
@@ -1,22 +1,30 @@
 package analysis
 
+import java.io.FileReader
+import java.io.File
+
 import grammar._
 import data._
 
-/** Lexer that splits input string into token list.
+/** Lexer that splits input file into token list.
   *
-  * @param string Input string with code.
+  * @param string Name of input file with code.
   */
-class Lexer(string: String) {
-  if (string == null || string.isBlank()) {
+class Lexer(filename: String) {
+  if (filename == null || filename.isBlank()) {
     throw new Exception("Invalid input string")
   }
 
-  /** Split input string into token list.
+  /** Split input file into token list.
     *
     * @return List of tokens.
     */
   def getTokens(): List[String] = {
-    string.split("( +)|(\n)").toList
+    val source = scala.io.Source.fromFile(filename)
+    val lines =
+      try source.mkString
+      finally source.close()
+
+    lines.split("( +)|(\n)").toList
   }
 }
